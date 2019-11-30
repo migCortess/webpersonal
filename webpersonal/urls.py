60ad6c234ba8fa16bef771f0eeb739ed44939dfe
Hector Miguel Cortes Ramos <hector.cortes@monterrey.gob.mx>
2019-11-30 00:26:53 -0600

    servir archivos media

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [
     path('contact/', views.contact, name="contact"),
     path('portfolio/', views.portfolio, name="portfolio"),
@@ -25,3 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
